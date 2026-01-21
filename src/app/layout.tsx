7e99965234397d8d00e50e881a8d8288e55bf375
Nipuna <k.nipuna.dasun@gmail.com>
2026-01-21 13:02:12 +0530

    myine v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,20 +41,20 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MH8VKKGB');`,
+})(window,document,'script','dataLayer','GTM-K65L3XNL');`,
           }}
         />
         {/* End Google Tag Manager */}
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MH8VKKGB"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K65L3XNL"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-        {/* End Google Tag Manager (noscript) */}
+        {/* End Google Tag Manager (noscriptddddd */}
         <AuthProvider>
           <CartProvider>
             {/* <MarqueeBar /> */}
