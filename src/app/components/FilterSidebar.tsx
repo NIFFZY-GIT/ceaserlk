eff60def5bb136637d09fe94d2b8e8e789696574
Nipuna <k.nipuna.dasun@gmail.com>
2026-03-22 12:21:39 +0530

    v7

--- src/app/components/FilterSidebar.tsx
+++ src/app/components/FilterSidebar.tsx
@@ -193,7 +193,7 @@ const FilterSidebar: React.FC<FilterSidebarProps> = ({
   return (
     <>
       {/* Desktop Sidebar */}
-      <aside className={`${showDesktop ? 'hidden lg:block lg:col-span-1 lg:self-start lg:h-fit lg:sticky lg:top-24' : 'hidden'}`}>
+      <aside className={`${showDesktop ? 'hidden lg:block lg:col-span-1 lg:self-start lg:h-fit lg:sticky lg:top-[max(8rem,calc(50vh-170px))]' : 'hidden'}`}>
         <div>
           <div className="bg-white border border-[#e5e5e5] p-6">
             <div className="flex items-center justify-between pb-4 mb-6 border-b border-[#e5e5e5]">
