7f3c98dd6fe2f4d5ccb1bbce1161c71f548d0fe6
Nipuna <k.nipuna.dasun@gmail.com>
2026-03-26 22:02:02 +0530

    tag1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,6 @@ import Footer from '@/app/components/Footer';
 import { SessionExpiryMonitor } from '@/components/SessionExpiryMonitor';
 import { SessionExpiredModal } from '@/components/SessionExpiredModal';
 
-// Optimize font loading with Next.js font system
 const montserrat = Montserrat({
   subsets: ['latin'],
   display: 'swap',
@@ -40,32 +39,81 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={montserrat.variable}>
       <body className={`bg-brand-black ${montserrat.className}`}>
-        {/* Google Tag Manager */}
+        
+        {/* --- Google Tag Manager Container 1 (GTM-MH8VKKGB) --- */}
         <Script
-          id="gtag-manager"
+          id="gtm-manager-1"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MH8VKKGB');`,
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MH8VKKGB');`,
           }}
         />
-        {/* End Google Tag Manager */}
-        {/* Google Tag Manager (noscript) */}
+
+        {/* --- Google Tag Manager Container 2 (GTM-N7WP5L48) --- */}
+        <Script
+          id="gtm-manager-2"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N7WP5L48');`,
+          }}
+        />
+
+        {/* --- Meta Pixel Code (1431696128992860) --- */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1431696128992860');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
+        {/* --- Noscript fallbacks for GTM and Meta Pixel --- */} 
         <noscript>
+          {/* GTM Container 1 */}
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-MH8VKKGB"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           />
+          {/* GTM Container 2 */}
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N7WP5L48"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+          {/* Meta Pixel */}
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1431696128992860&ev=PageView&noscript=1"
+            alt=""
+          />
         </noscript>
-        {/* End Google Tag Manager (noscript) */}
+
         <AuthProvider>
           <CartProvider>
-            {/* <MarqueeBar /> */}
             <SessionExpiryMonitor />
             <SessionExpiredModal />
             <Navbar />
